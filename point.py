29606928edcbea22a6f67524fecf8a00f2a67aee
ANAS-ST <106769613+ANAS-ST@users.noreply.github.com>
2022-10-28 21:30:00 +0200

    Update point.py

--- point.py
+++ point.py
@@ -1,28 +1,37 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
-bot = Client(
-    "point", api_id=13429252, api_hash="33d94e2daae5c7777f0d9b59a72ab0a6",
-    bot_token="5588598438:AAGG344SZ2_uP7z9QNwNGY_gf6r932YYNrs")
+bot = Client("point", api_id=13429252, api_hash="33d94e2daae5c7777f0d9b59a72ab0a6",
+             bot_token="5588598438:AAGG344SZ2_uP7z9QNwNGY_gf6r932YYNrs")
+
+channel = -1001235149288
 
 
 @bot.on_message(filters.command('start'))
-def command1(_, message):
-    bot.send_message(message.chat.id, "مرحبا بك في بوت point")
+async def command1(_, message):
+    await bot.send_message(message.chat.id, "مرحبا بك في بوت point")
 
 
-botton = [
-    [InlineKeyboardButton('-', 'm'),
-     InlineKeyboardButton('+', 'p')]
-]
-
+def get_keyboard(mid):
+    button = [
+        [InlineKeyboardButton('-', f'm-{mid}'),
+         InlineKeyboardButton('+', f'p-{mid}')],
+        [InlineKeyboardButton('-5', f'm5-{mid}'),
+         InlineKeyboardButton('+5', f'p5-{mid}')],
+        [InlineKeyboardButton('-10', f'm10-{mid}'),
+         InlineKeyboardButton('+10', f'p10-{mid}')],
+        [InlineKeyboardButton('zero', f'z-{mid}')]
+    ]
+    return button
 
 
 @bot.on_message()
-def command2(_, message):
+async def command2(_, message):
     text = message.text + '\nرصيدك هو :  0'
-    reply_markup = InlineKeyboardMarkup(botton)
-    message.reply(
+    msg = await bot.send_message(channel, text)
+    buttons = get_keyboard(msg.id)
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply(
         text=text,
         reply_markup=reply_markup,
         disable_web_page_preview=True
@@ -31,19 +40,30 @@ def command2(_, message):
 
 @bot.on_callback_query()
 async def callback(_, cb: CallbackQuery):
-	await cb.answer()
-	data = cb.message.text.split(':')
-	name = data[0].strip()
-	score = int(data[1].strip())
-	if cb.data == 'm' and score > 0:
-		score -= 1
-	elif cb.data == 'p':
-		score += 1
-	else:
-		return 
-	text = name + ':  ' + str(score)
-	await cb.message.edit_text(text, reply_markup=cb.message.reply_markup)
-		
-	
+    await cb.answer()
+    data = cb.message.text.split(':')
+    name = data[0].strip()
+    score = int(data[1].strip())
+    mode, mid = cb.data.split("-")
+    if mode == 'm' and score > 0:
+        score -= 1
+    elif mode == 'p':
+        score += 1
+    elif mode == 'm5' and score > 5:
+        score -= 5
+    elif mode == 'p5':
+        score += 5
+    elif mode == 'm10' and score > 10:
+        score -= 10
+    elif mode == 'p10':
+        score += 10
+    elif mode == 'z':
+        score = 0
+    else:
+        return
+    text = name + ':  ' + str(score)
+    await bot.edit_message_text(channel, int(mid), text)
+    await cb.message.edit_text(text, reply_markup=cb.message.reply_markup)
+
 
 bot.run()
